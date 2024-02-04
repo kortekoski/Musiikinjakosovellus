8ecda5dfbd784f20d3cfa143f852b5a89e5aecbc
kortekoski <mika.kortekoski@gmail.com>
2024-02-04 21:09:10 +0200

    tweak schema.sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Users (
     id SERIAL PRIMARY KEY, 
-    username TEXT, 
+    username TEXT UNIQUE, 
     password TEXT, 
     admin BOOLEAN DEFAULT FALSE);
 
@@ -11,26 +11,29 @@ CREATE TABLE Genres (
 CREATE TABLE Tracks (
     id SERIAL PRIMARY KEY, 
     name TEXT, 
-    user_id INTEGER REFERENCES Users, 
-    genre_id INTEGER REFERENCES Genres, 
+    user_id INTEGER REFERENCES Users ON DELETE CASCADE ON UPDATE CASCADE, 
+    genre_id INTEGER REFERENCES Genres ON DELETE CASCADE ON UPDATE CASCADE, 
     date TIMESTAMP, 
-    data BYTEA);
+    data BYTEA,
+    visible BOOLEAN DEFAULT TRUE,
+    private BOOLEAN);
 
 CREATE TABLE Versions (
     id SERIAL PRIMARY KEY, 
     version_number INTEGER, 
-    track_id INTEGER REFERENCES Tracks);
+    track_id INTEGER REFERENCES Tracks ON DELETE CASCADE);
 
 CREATE TABLE Keywords (
     id SERIAL PRIMARY KEY, 
     content TEXT);
 
 CREATE TABLE KeywordsTracks (
-    keyword_id INTEGER REFERENCES Keywords, 
-    track_id INTEGER REFERENCES Tracks);
+    keyword_id INTEGER REFERENCES Keywords ON DELETE CASCADE, 
+    track_id INTEGER REFERENCES Tracks ON DELETE CASCADE);
 
 CREATE TABLE Comments (
     id SERIAL PRIMARY KEY, 
-    content TEXT, date TIMESTAMP, 
-    track_id INTEGER REFERENCES Tracks, 
+    content TEXT, 
+    date TIMESTAMP, 
+    track_id INTEGER REFERENCES Tracks ON DELETE CASCADE, 
     user_id INTEGER REFERENCES Users);
