848fa413038af96f4bad20b6a8d911f1434f2824
kortekoski <mika.kortekoski@gmail.com>
2024-02-04 19:44:03 +0200

    Add Fly.io config

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mikomi/Musiikinjakosovellus/venv"
+set -gx VIRTUAL_ENV "/home/mikomi/TSOHA/Musiikinjakosovellus/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
