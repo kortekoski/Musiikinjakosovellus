f1f1528a243a294a90007ea93bd958be0bc63431
kortekoski <mika.kortekoski@gmail.com>
2024-02-07 13:31:19 +0200

    fix fly.io connection

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 
 import routes
 
